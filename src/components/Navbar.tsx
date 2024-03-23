1801ca028020ea4787764493c6a9bd4d08c233d8
AlexMuia31 <alexmuia31@gmail.com>
2024-03-23 11:30:36 +0300

    nav start

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <Image src="/assets/images/apple.svg" height={20} width={20} alt="" />
+        <div>
+          {["Phones", "MacBooks", "Tablets"].map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
