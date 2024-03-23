1dd9c1b8f5ec0c88f7c4ebdcb1b6292014c70a26
AlexMuia31 <alexmuia31@gmail.com>
2024-03-23 11:44:17 +0300

    nav continuation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,22 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-center items-center">
+      <nav className="flex w-full screen-max-width">
         <Image src="/assets/images/apple.svg" height={20} width={20} alt="" />
-        <div>
+        <div className="flex flex-1 justify-center max-sm:hidden">
           {["Phones", "MacBooks", "Tablets"].map((item, index) => (
             <div key={index}>{item}</div>
           ))}
+        </div>
+        <div>
+          <Image
+            src="/assets/images/search.svg"
+            height={20}
+            width={20}
+            alt=""
+          />
+          <Image src="/assets/images/bag.svg" height={20} width={20} alt="" />
         </div>
       </nav>
     </header>
